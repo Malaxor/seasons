29292e85ec0c9ab6d5b37d7619228e4291f23191
PHILIP COSTACHE <nogobaduk@gmail.com>
2020-05-19 11:50:08 -0400

    App is now class-based component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-   return <div>Hi there</div>;
+class App extends Component {
+   render() {
+      window.navigator.geolocation.getCurrentPosition(
+         position => console.log(position),
+         err => console.log(err)
+      );
+      return (
+         <div>Latitude: </div>
+      )
+   }
 }
+
 ReactDOM.render(<App />, document.querySelector('#root'));
